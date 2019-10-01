37c214be2c74a4e43cadc9c821bc24ec572cc349
0xCBA <52107128+0xCBA@users.noreply.github.com>
2019-10-01 15:56:03 -0400

    Create tempconv1.py

---
+++ tempconv1.py
@@ -0,0 +1,9 @@
+lower = 0  # lower limit of temperature table
+upper = 300  # upper limit
+step = 20  # step size
+
+fahr = lower
+while fahr <= upper:
+    celsius = 5 * (fahr - 32) / 9
+    print("{}\t{}".format(fahr, celsius)
+    fahr = fahr + step
